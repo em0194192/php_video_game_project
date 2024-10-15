8118edfb0a40d76e6fd43ec2d4afe1a349cc036b
Eric Miller <em0194192@otc.edu>
2024-10-14 22:03:20 -0500

    first commit

---
+++ view/show_login_form.php
@@ -0,0 +1,13 @@
+<form action="." method="post" class="border rounded shadow p-4">
+    <div class="form-group mb-3">
+        <label for="usernamefield" class="form-label">Username</label>
+        <input type="text" class="form-control" name="username" placeholder="Username" id="usernamefield">
+    </div>
+    <div class="form-group mb-3">
+        <label for="passwordfield" class="form-label">Password</label>
+        <input type="password" class="form-control" name="password" placeholder="Password" id="passwordfield">
+    </div>
+
+    <input type="submit" class="btn btn-primary" value="Sign In">
+</form>
+
---
+++ view/show_video_games.php
@@ -0,0 +1,29 @@
+<div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Title:</h5>
+      <p class="card-text">Genre:</p>
+      <p class="card-text">Platform:</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Title:</h5>
+      <p class="card-text">Genre:</p>
+      <p class="card-text">Platform:</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Title:</h5>
+      <p class="card-text">Genre:</p>
+      <p class="card-text">Platform:</p>
+    </div>
+  </div>
+</div>
+
+<!--https://getbootstrap.com/docs/5.3/components/card/-->
+
