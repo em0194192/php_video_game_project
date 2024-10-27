b2731bb0b5ada11ec62a2dbedd7112193249453c
Eric Miller <em0194192@otc.edu>
2024-10-26 23:11:23 -0500

    Minor Comments and tweaks

--- view/login_form.php
+++ view/login_form.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <style>
@@ -29,9 +29,12 @@
 </head>
 <body>
     <?php
+    //start session for access to session variables
     session_start();
+    //check query string for error message via get request (from login.php header url on checkLogin returning false)
     $errorMessage = isset($_GET['error']) ? htmlspecialchars($_GET['error']) : '';
     ?>
+
     <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
         <form action="../controller/login.php" method="post" class="content border rounded shadow p-4">
             <h1 style="text-align: center">Login</h1>
@@ -51,7 +54,8 @@
                 }
             ?>
         </form>
-    </div>           
+    </div>
+
 </body>
 </html>
 
